0bb36d3729a0b7f1ecc792c554c6fe3865b6abad
OKTAY KARA <oktay.kara@manas.com.tr>
2023-11-21 18:25:42 +0300

    Adding SFG Brewery Application

---
+++ src/main/java/guru/springframework/brewery/domain/BaseEntity.java
@@ -0,0 +1,39 @@
+package guru.springframework.brewery.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.*;
+
+import jakarta.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+
+    public BaseEntity(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate) {
+        this.id = id;
+        this.version = version;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false )
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+}
